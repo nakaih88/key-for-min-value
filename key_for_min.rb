e969972752e8950d6a6e573245d5f11aeb0f6e1c
Nakai <nakai.hamlin18@gmail.com>
2021-03-01 21:03:30 -0500

    commit tree bfe7cc10651f5f1914817ff2b06751e5a62add88

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_val = nil
+    min_key = nil
+    name_hash.each do |value, index|
+      if min_val == nil
+        min_val = index
+        min_key = value
+      elsif min_val > index
+        min_val = index
+        min_key = value
+      end
+    end
+    min_key
 end
